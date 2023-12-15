800e5f12f0052dcc19ffc39ffb132b725b4ac729
Abelhard Jauwena <109923732+ajauwena@users.noreply.github.com>
2023-12-14 21:58:54 -0500

    a_06.2_reference_genome_indexer.sh

--- a_06.2_reference_genome_indexer.sh
+++ a_06.2_reference_genome_indexer.sh
@@ -14,15 +14,3 @@ dir_barley_reference_genome="/home/ajauwena/scratch/binf_6999/barley_reference_g
 samtools faidx ${dir_barley_reference_genome}/GCA_904849725.1.fasta
 
 bwa index -p ${dir_barley_reference_genome}/GCA_904849725.1.fasta.gz -a bwtsw ${dir_barley_reference_genome}/GCA_904849725.1.fasta.gz
-
-# Maybe also use "bowtie2-build."
-
-# ----- Mohamad's -----
-
-#module load bwa samtools bowtie2
-
-#samtools faidx barley_refseq.fa
-
-#bwa index -p barley_refseq.fa.gz -a bwtsw barley_refseq.fa.gz
-
-#bowtie2-build barley_refseq.fa /home/melian/scratch/barley_feb_2023/barley_index/barley_refseq
